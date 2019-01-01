00036c80bd7004240d8df2ef196d08929807c212
Hoang Nguyen <phuonghoang713@gmail.com>
2019-01-01 17:44:09 +0700

    finish part 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
+  before_action :current_user, only: [:show, :update, :edit, :detroy]
+
   def show
-    @current_user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @current_user.update(user_params)
+    redirect_to @current_user
+  end
+
+  private
+
+  def current_user
+    @current_user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :name, :website,
+                                 :bio, :email, :phone, :gender)
   end
 end
