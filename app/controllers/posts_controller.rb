1a7bd435739deeaec29a8d61aa7ee7f3e2fbcdb1
Hoang Nguyen <phuonghoang713@gmail.com>
2019-01-06 02:58:25 +0700

    can save image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,25 @@
 class PostsController < ApplicationController
   def create
-    Post.create(post_params)
+    p "asdasdads cerate #{params}"
+    post = Post.create(post_params)
+    post.save!
     redirect_to root_path
   end
 
   def new
+    p "asdasdads new #{params}"
     @post = Post.new
   end
 
   private
 
   def post_params
+    p "asdjgsakhfgakshdfgkjasfgkjsgkffsdfs"
+    p "current user #{current_user.id}"
+    p "asdasdads #{params[:description]}"
+    p "asdasdads #{params[:image]}"
+    p "asdasdads124c #{params[:user_id]}"
+    p "======================================"
     params.require(:post).permit(:description, :image, :user_id)
   end
 end
